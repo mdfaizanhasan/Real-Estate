aafadff57764d953ccaf77c593ec1602ad3f4224
faizanhasan <mdfaizanalam367@gmail.com>
2023-10-08 16:34:04 +0530

    create sign up api router

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRouter from './routes/user.route.js';
-// import authRouter from './routes/auth.route.js';
+import authRouter from './routes/auth.route.js';
 // import listingRouter from './routes/listing.route.js';
 
 dotenv.config();
@@ -16,10 +16,12 @@ mongoose.connect(process.env.MONGO).then(() => {
 
 const app = express();
 
+app.use(express.json());
+
 app.listen(3000, () => {
     console.log(`Server is running on the port 3000!`)
 });
 
 app.use('/server/user', userRouter);
-// app.use('/server/auth', authRouter);
+app.use('/server/auth', authRouter);
 // app.use('/server/listing', listingRouter);
